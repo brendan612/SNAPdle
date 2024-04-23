c8d220a6751e671a1cec692a5dfce27280bc8d75
Brendan <brendan612@gmail.com>
2024-04-22 17:58:16 -0700

    Added create table scripts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,4 @@
 import express, { Express } from 'express';
-import ExpressMongoSanitize from 'express-mongo-sanitize';
 import compression from 'compression';
 import cors from 'cors';
 import routes from './routes';
@@ -15,9 +14,6 @@ app.use(express.json());
 
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
-
-// sanitize request data
-app.use(ExpressMongoSanitize());
 
 // gzip compression
 app.use(compression());
